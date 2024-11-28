91870408a38449e01cc5b002184ad983a3ce5063
songocu <songocu_tud@yahoo.com>
2024-11-28 13:42:03 +0200

    First commit

---
+++ src/main/java/com/tudor/trythinks/TrythinksApplication.java
@@ -0,0 +1,13 @@
+package com.tudor.trythinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrythinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrythinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tudor/trythinks/TrythinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudor.trythinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrythinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
